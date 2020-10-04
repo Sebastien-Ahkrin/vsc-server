f802dcdc34637cece833edb8a2de2a33ab5caff4
Sebastien Ahkrin <sebastien48criquet@gmail.com>
2020-10-04 19:31:41 +0200

    feat: read package.json script in webview

--- src/TreeServerProvider.ts
+++ src/TreeServerProvider.ts
@@ -1,16 +1,31 @@
-import { TreeDataProvider, TreeItem, TreeItemCollapsibleState } from 'vscode';
+import { TreeDataProvider, TreeItem, TreeItemCollapsibleState, window, WorkspaceFolder } from 'vscode';
+import { promises as fs } from 'fs';
+import { join } from 'path';
 
 export class TreeServerProvider implements TreeDataProvider<ServerItem> {
  
+  public constructor(private workspace: string) {}
+
   public getTreeItem(item: ServerItem): TreeItem {
     return item;
   }
 
-  public getChildren(element: ServerItem): ServerItem[] {
-    return [
-      new ServerItem('Hello 1', TreeItemCollapsibleState.None),
-      new ServerItem('Hello 2', TreeItemCollapsibleState.None)
-    ];
+  public async getChildren(): Promise<Array<ServerItem>> {
+    try {
+      return Object.keys(await this.getScripts()).map((script) => {
+        return new ServerItem(script, TreeItemCollapsibleState.None);
+      });
+    } catch (error) {
+      window.showInformationMessage('Cannot find package.json file');
+      return [];
+    }
+  }
+
+  private async getScripts() {
+    const result = await fs.readFile(join(this.workspace, 'package.json'), { encoding: 'utf-8' });
+    const json = JSON.parse(result);
+
+    return json.scripts || [];
   }
 
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,11 @@
-import { ExtensionContext, commands, window } from 'vscode';
+import { ExtensionContext, workspace, window } from 'vscode';
 import { TreeServerProvider } from './TreeServerProvider';
 
-interface Command {
-	command: string,
-	action: () => void;
-}
-
-function register(context: ExtensionContext, command: Command): void {
-	const cmd = commands.registerCommand(command.command, command.action);
-	context.subscriptions.push(cmd);
-}
-
 export function activate(context: ExtensionContext) {
-	console.log('Congratulations, your extension "vsc-server" is now active!');
-
-	register(context, {
-		command: 'vsc-server.helloWorld',
-		action: () => {
-			window.showInformationMessage("Hello, World!");
-		}
-	});
-
-	window.registerTreeDataProvider('serverView', new TreeServerProvider());
+	const paths = workspace.workspaceFolders;
+	if (paths !== undefined) {
+		window.registerTreeDataProvider('serverView', new TreeServerProvider(paths[0].uri.fsPath));
+	}
 }
 
 export function deactivate() {}
